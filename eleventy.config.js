f95a98246dfb669b1d70f9832411fd56233dfac2
Joshua Blount <hi@joshuablount.com>
2024-07-10 10:29:03 -0700

    integrate with webc in a very hacky way

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,12 @@
 // Import prior to `module.exports` within `.eleventy.js`
 import { DateTime } from "luxon";
+import pluginWebc from "@11ty/eleventy-plugin-webc";
 
 export default function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./_components/**.*");
+  eleventyConfig.addPlugin(pluginWebc);
+  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addFilter("nicerDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATETIME_HUGE_WITH_SECONDS);
   });
