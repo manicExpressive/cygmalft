4f1fd8f6c4e62e80140a70d2b9f147785f6ae494
Joshua Blount <hi@joshuablount.com>
2024-07-10 18:45:45 -0700

    haphazard headline pull

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,11 +4,15 @@ import pluginWebc from "@11ty/eleventy-plugin-webc";
 
 export default function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./_components/**.*");
-  eleventyConfig.addPlugin(pluginWebc);
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: "_components/**/*.webc",
+  });
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("open-graph.png");
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addFilter("nicerDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATETIME_HUGE_WITH_SECONDS);
+    return DateTime.fromJSDate(dateObj).toLocaleString(
+      DateTime.DATETIME_HUGE_WITH_SECONDS,
+    );
   });
 }
---
+++ pullHeadlines.js
@@ -0,0 +1,40 @@
+import puppeteer from "puppeteer";
+import cheerio from "cheerio";
+import fs from "fs";
+
+(async () => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto("https://oregonlive.com", {
+    waitUntil: "networkidle0",
+  });
+
+  // Set screen size
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  const content = await page.content();
+  const htmlContent = await page.content();
+
+  let stringText = "<ul>";
+  let listOfUrls = [];
+  const $ = cheerio.load(htmlContent);
+  $(".subscriberExclusive").each((index, element) => {
+	let url = $(element).closest("a").attr().href;
+    	let processedUrl = new URL(url + "?outputType=amp&message=fuckYourPaywall");
+	listOfUrls.pop(processedUrl);	
+	stringText = stringText + `<li><a href="${processedUrl}">${processedUrl}</a></li>\n`;
+    console.log(listOfUrls);
+  });
+
+  stringText = stringText + `</ul>`;
+
+fs.writeFileSync("_components/ol-headlines.webc", stringText)
+
+  await browser.close();
+})();
